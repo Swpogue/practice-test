63f13f2be6891e8ee1c86ee8b5d0a1a087d7bb63
Swpogue <swpogue02@gmail.com>
2023-04-03 17:35:03 -0600

    js done

--- app.js
+++ app.js
@@ -16,6 +16,44 @@ const cases = [
   { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
 ]
 
-// YOUR JS CODE
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  console.log(`${currentCase.title} ${currentCase.releaseYear}`)
+}
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  if (currentCase.releaseYear == 1903) {
+    console.log(`${currentCase.title}`)
+  }
+}
+
+var currentYear;
+currentYear = 2023;
+
+var jsReleaseYear;
+jsReleaseYear = 1995;
+
+console.log("current age of JavaScript is " + (currentYear - jsReleaseYear))
+
+if (currentYear - jsReleaseYear >= ageToVote) {
+  console.log(`JavaScript is old enough to vote`)
+}
+if (currentYear - jsReleaseYear < ageToVote) {
+  console.log(`JavaScript is not old enough to vote`)
+}
+
+console.log("Starting")
+for (let i = 0; i <= 100; i += 10) {
+  console.log(i)
+  if (i == 50) {
+    console.log("halfway")
+  }
+}
+console.log("Finished")
+
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
 
 
+// **Working with an array of objects in JavaScript**
+// Iterate over the `cases` array and log the title and release year of each story
+//- Iterate over the `cases` array and log only the title of cases released in the year `1903`
