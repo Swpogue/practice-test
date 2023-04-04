e8ebea5cdd0af2ff9763acd931d4f0dc61e45858
Swpogue <swpogue02@gmail.com>
2023-04-04 14:03:46 -0600

    JS redo

--- app.js
+++ app.js
@@ -16,39 +16,32 @@ const cases = [
   { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
 ]
 
-for (let i = 0; i < cases.length; i++) {
-  const currentCase = cases[i];
-  console.log(`${currentCase.title} ${currentCase.releaseYear}`)
-}
-for (let i = 0; i < cases.length; i++) {
-  const currentCase = cases[i];
-  if (currentCase.releaseYear == 1903) {
-    console.log(`${currentCase.title}`)
-  }
-}
-
-var currentYear;
-currentYear = 2023;
-
-var jsReleaseYear;
-jsReleaseYear = 1995;
-
-console.log("current age of JavaScript is " + (currentYear - jsReleaseYear))
+var currentYear = 2023
+var jsReleaseYear = 1995
+console.log("current age of JavaScript is " + (currentYear - jsReleaseYear));
 
 if (currentYear - jsReleaseYear >= ageToVote) {
-  console.log(`JavaScript is old enough to vote`)
+  console.log("JavaScript is old enough to vote");
 }
 if (currentYear - jsReleaseYear < ageToVote) {
-  console.log(`JavaScript is not old enough to vote`)
+  console.log("JavaScript is not old enough to vote");
 }
-
-console.log("Starting")
+console.log("Starting");
 for (let i = 0; i <= 100; i += 10) {
-  console.log(i)
+  console.log(i);
   if (i == 50) {
-    console.log("halfway")
+    console.log("Halfway");
   }
 }
-console.log("Finished")
+console.log("Finished");
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`);
 
-console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  console.log(`${currentCase.title} ${currentCase.releaseYear}`);
+}
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  if (currentCase.releaseYear == 1903)
+    console.log(`${currentCase.title}`);
+}
